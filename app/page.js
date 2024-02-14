84d33a68e91da7eff118824b5011bd071df23af4
eleahburman <eleahjburman@gmail.com>
2024-02-14 14:27:05 -0500

    Add container to header

--- app/page.js
+++ app/page.js
@@ -2,31 +2,33 @@ import  {ImStatsBars} from 'react-icons/im'
 
 export default function Home() {
   return (
-    <header className="flex items-center justify-between">
-      { /* User information */ }
-      <div className="flex items-center gap-2">
-        { /* User avatar */ }
-        <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
-          <img
-            className="object-cover w-full h-full"
-            src= "/eleah.jpg" 
-            alt="User avatar" 
-          />
+    <header className="container max-w-2xl px-6 py-6 mx-auto">
+      <div className="flex items-center justify-between">
+        { /* User information */ }
+        <div className="flex items-center gap-2">
+          { /* User avatar */ }
+          <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+            <img
+              className="object-cover w-full h-full"
+              src= "/eleah.jpg" 
+              alt="User avatar" 
+            />
+          </div>
+          { /* User name */ }
+          <small>Hi, Eleah!</small>
         </div>
-        { /* User name */ }
-        <small>Hi, Eleah!</small>
+        { /* User navigation */ }
+        <nav className="flex items-center gap-2">
+          <div>
+            <ImStatsBars className="text-2xl"/>
+          </div>
+          <div>
+            <button className="btn btn-danger">
+              Sign Out
+            </button>
+          </div>
+        </nav>
       </div>
-      { /* User navigation */ }
-      <nav className="flex items-center gap-2">
-        <div>
-          <ImStatsBars className="text-2xl"/>
-        </div>
-        <div>
-          <button className="btn btn-danger">
-            Sign Out
-          </button>
-        </div>
-      </nav>
     </header>
   );
 }
