280f64e40e8e8621660d4f56f9b6f14f69d5df16
eleahburman <eleahjburman@gmail.com>
2024-02-14 16:26:32 -0500

    Create expense item component and data

--- app/page.js
+++ app/page.js
@@ -1,6 +1,65 @@
+//import components
+import ExpenseItem from "@/components/ExpenseItem";
+
+//import utils
+import { currencyFormatter } from "@/library/utils";
+
+const DUMMY_EXPENSES = [
+  {
+    id: 1,
+    title: "Entertainment",
+    color: "yellow",
+    amount: 500,
+  },
+  {
+    id: 2,
+    title: "Groceries",
+    color: "blue",
+    amount: 1000,
+  },
+  {
+    id: 3,
+    title: "Travel",
+    color: "green",
+    amount: 5000,
+  },
+  {
+    id: 4,
+    title: "Clothes",
+    color: "pink",
+    amount: 300,
+  }
+]
 
 export default function Home() {
   return (
-    <p>Hello</p>
+    <main className="container max-w-2xl px-6 mx-auto">
+      <section className="py-3">
+        <small className="text-gray-400 text-md">
+          My Balance
+        </small>
+        <h2 className="text-4xl font-bold">
+          { currencyFormatter(10000)}
+        </h2>
+      </section>
+      <section className="flex items-center gap-2 py-3">
+        <button className="btn btn-primary">+ Expenses</button>
+        <button className="btn btn-primary-outline">+ Income</button>
+      </section>
+      {/* Expense List */}
+      <section className="py-6">
+        <h3 className="text-2xl">My Expenses</h3>
+        <div className="flex flex-col gap-4 mt-6">
+          {DUMMY_EXPENSES.map((expense) => { return (
+            <ExpenseItem 
+              color={expense.color}
+              title={expense.title}
+              amount={expense.amount}
+            />
+          )
+        })}
+        </div>
+      </section>
+    </main>
   );
 }
