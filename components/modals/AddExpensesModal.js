58cde0c23ab178be255e1c773e3e7535dd3cc07e
eleahburman <eleahjburman@gmail.com>
2024-03-08 15:51:23 -0500

    Create expense input for modal

--- components/modals/AddExpensesModal.js
+++ components/modals/AddExpensesModal.js
@@ -1,9 +1,25 @@
+//modal import
 import Modal from '@/components/Modal'
 
+//react imports
+import { useState } from 'react';
+
 function AddExpensesModal({show, onClose}){
+  const [expenseAmount, setExpenseAmount]= useState("");
   return(
     <Modal show={show} onClose={onClose}>
-      <h3>Hello, I am an expense modal</h3>
+      <div className="flex flex-col gap-4">
+        <label>Enter an amount...</label>
+        <input 
+          type="number"
+          min={0.01}
+          step={0.01}
+          placeholder="Enter expense amount"
+          value={expenseAmount}
+          onChange={(e)=>{setExpenseAmount(e.target.value)}}
+          required
+        />
+      </div>
     </Modal>
   );
 }
