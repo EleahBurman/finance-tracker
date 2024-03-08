d66922bb10a4c4fb941e36c0a2cb54d5472e26f5
eleahburman <eleahjburman@gmail.com>
2024-03-08 16:22:43 -0500

    Make expense modal dynamic

--- components/modals/AddExpensesModal.js
+++ components/modals/AddExpensesModal.js
@@ -5,11 +5,37 @@ import Modal from '@/components/Modal'
 import { useState, useContext } from 'react';
 import { financeContext } from '@/library/store/finance-context';
 
+//uuid imports
+import { v4 as uuidv4 } from 'uuid';
+
 function AddExpensesModal({show, onClose}){
   const [expenseAmount, setExpenseAmount]= useState("");
   const [selectedCategory, setSelectedCategory]= useState(null);
   const { expenses } = useContext(financeContext);
   
+  const addExpenseItemHandler = () => {
+    const expense = expenses.find(e=>{
+      return e.id === selectedCategory
+    })
+    const newExpense = {
+      color:expense.color,
+      title:expense.title,
+      total:expense.total + +expenseAmount,
+      items: [
+        ...expense.items,
+        {
+          amount: +expenseAmount,
+          createdAt: new Date(),
+          id: uuidv4(),
+        }
+      ]
+    }
+
+    console.log(newExpense)
+    setExpenseAmount("");
+    setSelectedCategory(null);
+    onClose();
+  }
   return(
     <Modal show={show} onClose={onClose}>
       <div className="flex flex-col gap-4">
@@ -26,29 +52,44 @@ function AddExpensesModal({show, onClose}){
       </div>
 
       {/* Add expense categories */}
-      <div className="flex flex-col gap-4 mt-6">
-        {expenses.map((expense)=>{ 
-          return(
-            <button
-              key={expense.id}
-              onClick={()=>{setSelectedCategory(expense.id)}}
-            >
-              <div style={{
-                boxShadow: expense.id === selectedCategory ? "1px 1px 4px" : "none"
-              }}className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
-                <div className="flex items-center gap-2">
-                  {/*Colored Circle*/}
-                  <div 
-                    className="w-[25px] h-[25px] rounded-full" 
-                    style={{backgroundColor: expense.color}}
-                  />
-                  <h4 className="capitalize">{expense.title}</h4>
+      {expenseAmount > 0 && (
+        <div className="flex flex-col gap-4 mt-6">
+          {expenses.map((expense)=>{ 
+            return(
+              <button
+                key={expense.id}
+                onClick={()=>{setSelectedCategory(expense.id)}}
+              >
+                <div style={{
+                  boxShadow: expense.id === selectedCategory ? "1px 1px 4px" : "none"
+                }}className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
+                  <div className="flex items-center gap-2">
+                    {/*Colored Circle*/}
+                    <div 
+                      className="w-[25px] h-[25px] rounded-full" 
+                      style={{backgroundColor: expense.color}}
+                    />
+                    <h4 className="capitalize">{expense.title}</h4>
+                  </div>
                 </div>
-              </div>
-            </button>
-          )
-        })}
-      </div>
+              </button>
+            )
+          })}
+        </div>
+      )}
+
+      {/* Add submit expense button */}
+      {expenseAmount > 0 && selectedCategory && (
+        <div className="mt-6">
+          <button 
+            className="btn btn-primary"
+            onClick={addExpenseItemHandler}
+          >
+            Add Expense    
+          </button>
+        </div>  
+      )}
+      
     </Modal>
   );
 }
