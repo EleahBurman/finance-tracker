a4cb9f512af00217a1e5dafdd9f8b6de33bb4184
eleahburman <eleahjburman@gmail.com>
2024-03-08 16:07:10 -0500

    Set box shadow if expense is clicked on

--- components/modals/AddExpensesModal.js
+++ components/modals/AddExpensesModal.js
@@ -2,10 +2,14 @@
 import Modal from '@/components/Modal'
 
 //react imports
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { financeContext } from '@/library/store/finance-context';
 
 function AddExpensesModal({show, onClose}){
   const [expenseAmount, setExpenseAmount]= useState("");
+  const [selectedCategory, setSelectedCategory]= useState(null);
+  const { expenses } = useContext(financeContext);
+  
   return(
     <Modal show={show} onClose={onClose}>
       <div className="flex flex-col gap-4">
@@ -19,6 +23,31 @@ function AddExpensesModal({show, onClose}){
           onChange={(e)=>{setExpenseAmount(e.target.value)}}
           required
         />
+      </div>
+
+      {/* Add expense categories */}
+      <div className="flex flex-col gap-4 mt-6">
+        {expenses.map((expense)=>{ 
+          return(
+            <button
+              key={expense.id}
+              onClick={()=>{setSelectedCategory(expense.id)}}
+            >
+              <div style={{
+                boxShadow: expense.id === selectedCategory ? "1px 1px 4px" : "none"
+              }}className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
+                <div className="flex items-center gap-2">
+                  {/*Colored Circle*/}
+                  <div 
+                    className="w-[25px] h-[25px] rounded-full" 
+                    style={{backgroundColor: expense.color}}
+                  />
+                  <h4 className="capitalize">{expense.title}</h4>
+                </div>
+              </div>
+            </button>
+          )
+        })}
       </div>
     </Modal>
   );
